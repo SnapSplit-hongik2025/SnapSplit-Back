5676fab448a1b1203123519358f01410d3874c2e
answodud <answodud0112@gmail.com>
2025-06-13 00:24:54 +0900

    Init: 프로젝트 초기 생성 및 기본 폴더 구조 추가

---
+++ src/main/java/com/snapsplit/backend/SnapSplitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snapsplit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapSplitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapSplitBackendApplication.class, args);
+    }
+
+}
