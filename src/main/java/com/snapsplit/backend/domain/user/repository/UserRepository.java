46975df2b7e9ce7c18fc737725762129780fab03
Jaeyoung Mun <answodud0112@gmail.com>
2025-07-05 16:49:06 +0900

    feat: 유저 검색 기능 구현

--- src/main/java/com/snapsplit/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/snapsplit/backend/domain/user/repository/UserRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // 카카오 아이디로 사용자 찾기
     Optional<User> findByKakaoId(String kakaoId);
+
+    // 유저 코드로 사용자 찾기
+    Optional<User> findByUserCode(String userCode);
 }
---
+++ src/main/java/com/snapsplit/backend/feature/createTrip/controller/TripMemberController.java
@@ -0,0 +1,38 @@
+package com.snapsplit.backend.feature.createTrip.controller;
+
+import com.snapsplit.backend.feature.auth.dto.KakaoUserResponse;
+import com.snapsplit.backend.feature.createTrip.dto.TripMemberResponse;
+import com.snapsplit.backend.feature.createTrip.service.TripMemberService;
+import com.snapsplit.backend.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class TripMemberController {
+
+    private final TripMemberService tripMemberService;
+
+    // 유저 코드로 유저 검색
+    @GetMapping("/code/{userCode}")
+    public ResponseEntity<ApiResponse<TripMemberResponse>> getUserByUserCode(
+            @PathVariable String userCode,
+            @AuthenticationPrincipal(expression = "id") Long currentUserId
+    ) {
+        TripMemberResponse response = tripMemberService.findByUserCode(userCode, currentUserId);
+
+        // 검색 결과가 존재하지 않아 비어있을 경우
+        if (response == null) {
+            return ResponseEntity.ok(ApiResponse.success("검색된 사용자가 없습니다.", null));
+        }
+
+        // 검색 결과가 존재할 경우
+        return ResponseEntity.ok(ApiResponse.success("사용자 조회 성공", response));
+    }
+}
