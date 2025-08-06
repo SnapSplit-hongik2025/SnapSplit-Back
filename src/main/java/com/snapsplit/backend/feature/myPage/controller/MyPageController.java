7000cf23c87aff115feb988af3c902649508e4ca
eunjung <enjung915@naver.com>
2025-08-06 15:30:02 +0900

    feat: 마이페이지 수정 방식 Multipart 방식으로 변경 및 S3 업로드 연동

--- src/main/java/com/snapsplit/backend/feature/myPage/controller/MyPageController.java
+++ src/main/java/com/snapsplit/backend/feature/myPage/controller/MyPageController.java
@@ -1,19 +1,24 @@
 package com.snapsplit.backend.feature.myPage.controller;
 
 import com.snapsplit.backend.domain.user.repository.UserRepository;
-import com.snapsplit.backend.feature.myPage.dto.MyPageUpdateRequest;
 import com.snapsplit.backend.feature.myPage.dto.MyPageResponse;
 import com.snapsplit.backend.feature.myPage.service.MyPageService;
 import com.snapsplit.backend.domain.user.entity.User;
 import com.snapsplit.backend.global.response.ApiResponse;
+import com.snapsplit.backend.global.s3.S3Uploader;
 import com.snapsplit.backend.global.security.CustomUserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class MyPageController {
 
     private final MyPageService myPageService;
     private final UserRepository userRepository;
+    private final S3Uploader s3Uploader;
 
     // GET /home/myPage
     // 마이페이지 조회
@@ -40,15 +46,22 @@ public class MyPageController {
     // PUT /home/myPage
     // 마이페이지 수정
     @Operation(summary = "마이페이지 수정", description = "기존 마이페이지 정보를 수정합니다.")
-    @PutMapping
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<Void>> updateProfile(
             @AuthenticationPrincipal CustomUserPrincipal principal,
-            @RequestBody MyPageUpdateRequest request
-    ) {
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) MultipartFile profileImage
+    ) throws IOException {
         Long userId = principal.getId();
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."));
-        myPageService.updateProfile(user, request.name(), request.profileImageUrl());
+
+        String profileImageUrl = null;
+        if (profileImage != null && !profileImage.isEmpty()) {
+            profileImageUrl = s3Uploader.upload(profileImage, "profile");
+        }
+
+        myPageService.updateProfile(user, name, profileImageUrl);
         return ResponseEntity.ok(ApiResponse.success("프로필 수정 완료", null));
     }
 }
