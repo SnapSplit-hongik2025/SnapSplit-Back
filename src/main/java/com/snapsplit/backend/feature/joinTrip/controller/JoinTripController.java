105e14d95b7a3ee2144c40413cbb0768d633a370
Jaeyoung Mun <answodud0112@gmail.com>
2025-09-12 18:28:34 +0900

    fix: joinTrip 인증 객체에서 userId를 조회하도록 로직 변경

--- src/main/java/com/snapsplit/backend/feature/joinTrip/controller/JoinTripController.java
+++ src/main/java/com/snapsplit/backend/feature/joinTrip/controller/JoinTripController.java
@@ -2,12 +2,13 @@ package com.snapsplit.backend.feature.joinTrip.controller;
 
 import com.snapsplit.backend.feature.joinTrip.dto.JoinTripRequest;
 import com.snapsplit.backend.feature.joinTrip.dto.JoinTripResponse;
-import com.snapsplit.backend.feature.joinTrip.dto.JoinTripResult;
 import com.snapsplit.backend.feature.joinTrip.service.JoinTripService;
 import com.snapsplit.backend.global.response.ApiResponse;
+import com.snapsplit.backend.global.security.CustomUserPrincipal;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +24,8 @@ public class JoinTripController {
 
     @Operation(summary = "초대 코드로 여행 참여", description = "참여하고 싶은 초대 코드를 입력하여 여행에 참여합니다.")
     @PostMapping("/join")
-    public ApiResponse<JoinTripResponse> joinTrip(@RequestBody JoinTripRequest request) {
-        JoinTripResponse response = joinTripService.joinTrip(request.getUserId(), request.getInviteCode());
+    public ApiResponse<JoinTripResponse> joinTrip(@RequestBody JoinTripRequest request, @AuthenticationPrincipal CustomUserPrincipal user) {
+        JoinTripResponse response = joinTripService.joinTrip(user.getId(), request.getInviteCode());
         return ApiResponse.success("여행에 성공적으로 참여가 완료되었습니다.", response);
     }
 }
--- src/main/java/com/snapsplit/backend/feature/joinTrip/dto/JoinTripRequest.java
+++ src/main/java/com/snapsplit/backend/feature/joinTrip/dto/JoinTripRequest.java
@@ -4,6 +4,5 @@ import lombok.Data;
 
 @Data
 public class JoinTripRequest {
-    private Long userId; // 유저 아이디
     private String inviteCode; // 초대 코드
 }
